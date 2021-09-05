3bb99ccda6341fa722207a5b730cb1a0125448c8
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-05 23:23:11 +0530

    Add files via upload

---
+++ increment aaray.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+/*	int n;
+	int i;
+	cin>>n;
+	int a[n];
+	for(i=0;i<n;i++)
+	cin>>a[i];
+	for(i=0;i<n;i++){
+	a[i]+=1;
+	cout<<a[i]<<" ";
+     }*/
+   /*  signed char ch;
+     ch=128;
+     cout<<ch;*/
+   /*  if(0)
+     {
+     	cout<<"hi";
+     	
+	 }
+	else
+	{
+		cout<<"bye";
+	}*/
+	int i=0;
+	while(i<3)
+	i++;
+	cout<<"in whole poop"<<i<<endl;
+	
+}
