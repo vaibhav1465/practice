6f792624f879eefa0748eddfcbba629d0dce07a3
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-13 23:28:06 +0530

    Add files via upload

---
+++ conio.cpp
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<dos.h>
+int main()
+{
+	printf("sbsbjbjkbf,bj\n");
+	clrscr();
+	getch();
+	printf("bjbfhfe f");
+}
+	  room_c=room[roomt];
+	  meal_c=meal[mealt];
+	   void VICTORIA()
+{
+	printf("\n\t\t\t\t*****\n\t\t\t\t  Victoria Hotel\n\t\t\t\t\t   C A N A D A\n\t\t\t\t*****\t\t");
+    getch();
+	printf("\n\n\t*\tModest rooms, suites & apartments in a casual hotel, offering a pool, a spa & 3 restaurants.");
+    printf("\n\t*\t3 restaurants and 3 cafes.\n\t*\tFree shuttle to the beach\n\t*\tBasketball and tennis courts\n\t*\tFitness centre\t\t\t");
+    getch();
+    printf("\n\n\t\t\t\tT Y P E   O F  R O O M S\n\t\t\t\t----------------------------");
+    printf("\n\t\t1\tBusiness Suite\tINR 14000/-per night");
+     printf("\n\t\t2\tExecutive Suite\tINR 175000/-per night");
+	  printf("\n\t\t3\tLuxury Suite\tINR 11000/-per night\t\t");   
+	  getch();
+	  printf("\n\n\t\t[PRICES WILL VARY WITH SEASONS]"); 
+	  printf("\n\n\t* Free wifi\t* 24X7 Room Service\t* GST EXTRA\t* Extra Bedding Available");
+	  getch();
+	 printf("\n\n\t\tSELECT YOUR ROOM TYPE\t");
+	 int room[4]={0,14000,17500,11000},roomt;
+	 scanf("%d",&roomt);
+	 printf("\n\t*\tENTER NO OF ROOMS REQUIRED\t");
+	 scanf("%d",&nrooms);
+	 printf("\n\n\t*\tANY EXTRA BEDDING\t");
+	 scanf("%d",&extrab);
+	  printf("\n\n\t\t->SELECT YOUR MEAL PLAN\t");
+	  int meal[5]={0,0,2000,7000,5500},mealt;
+	  printf("\n\t1-Europeon plan INR 0/-");
+	  printf("\n\n\t2-Continental plan INR 2000/-");
+	  printf("\n\n\t3-American plan INR 7000/-");
+	  printf("\n\n\t4-Modified American plan INR 5500/-\t\t");
+	  scanf("%d",&mealt);
+	  
+	  
+}
+int main()
+{
+	
+	VICTORIA();
+}
+[8:47 PM, 11/13/2019] Aniket (analytical Class): #include<stdio.h>
+#include<conio.h>
+int nrooms,extrab;
+void THE_OAKS_HOTEL()
+{
+	printf("\n\t\t\t\t*****\n\t\t\t\t  The Oaks Hotel\n\t\t\t\t\t   C A N A D A\n\t\t\t\t*****\t\t");
+    getch();
+	printf("\n\n\t*\tModest rooms, suites & apartments in a casual hotel, offering a pool, a spa & 3 restaurants.");
+    printf("\n\t*\t3 restaurants and 3 cafes.\n\t*\tFree shuttle to the beach\n\t*\tBasketball and tennis courts\n\t*\tFitness centre\t\t\t");
+    getch();
+    printf("\n\n\t\t\t\tT Y P E   O F  R O O M S\n\t\t\t\t----------------------------");
+    printf("\n\t\t1\tBusiness Suite\tINR 14000/-per night");
+     printf("\n\t\t2\tExecutive Suite\tINR 175000/-per night");
+	  printf("\n\t\t3\tLuxury Suite\tINR 11000/-per night\t\t");   
+	  getch();
+	  printf("\n\n\t\t[PRICES WILL VARY WITH SEASONS]"); 
+	  printf("\n\n\t* Free wifi\t* 24X7 Room Service\t* GST EXTRA\t* Extra Bedding Available");
+	  getch();
+	 printf("\n\n\t\tSELECT YOUR ROOM TYPE\t");
+	 int room[4]={0,14000,17500,11000},roomt;
+	 scanf("%d",&roomt);
+	 printf("\n\t*\tENTER NO OF ROOMS REQUIRED\t");
+	 scanf("%d",&nrooms);
+	 printf("\n\n\t*\tANY EXTRA BEDDING\t");
+	 scanf("%d",&extrab);
+	  printf("\n\n\t\t->SELECT YOUR MEAL PLAN\t");
+	  int meal[5]={0,0,2000,7000,5500},mealt;
+	  printf("\n\t1-Europeon plan INR 0/-");
+	  printf("\n\n\t2-Continental plan INR 2000/-");
+	  printf("\n\n\t3-American plan INR 7000/-");
+	  printf("\n\n\t4-Modified American plan INR 5500/-\t\t");
+	  scanf("%d",&mealt);
+	  
+	  
+}
+int main()
+{
+	
+	THE_OAKS_HOTEL();
+}
+[8:50 PM, 11/13/2019] Aniket (analytical Class): #include<stdio.h>
+#include<conio.h>
+int nrooms,extrab;
+void HOMEWOOD_SUITES()
+{
+	printf("\n\t\t\t\t*****\n\t\t\t\t  Homewood Suites \n\t\t\t\t\t   C A N A D A\n\t\t\t\t*****\t\t");
+    getch();
+	printf("\n\n\t*\tModest rooms, suites & apartments in a casual hotel, offering a pool, a spa & 3 restaurants.");
+    printf("\n\t*\t3 restaurants and 3 cafes.\n\t*\tFree shuttle to the beach\n\t*\tBasketball and tennis courts\n\t*\tFitness centre\t\t\t");
+    getch();
+    printf("\n\n\t\t\t\tT Y P E   O F  R O O M S\n\t\t\t\t----------------------------");
+    printf("\n\t\t1\tBusiness Suite\tINR 14000/-per night");
+     printf("\n\t\t2\tExecutive Suite\tINR 175000/-per night");
+	  printf("\n\t\t3\tLuxury Suite\tINR 11000/-per night\t\t");   
+	  getch();
+	  printf("\n\n\t\t[PRICES WILL VARY WITH SEASONS]"); 
+	  printf("\n\n\t* Free wifi\t* 24X7 Room Service\t* GST EXTRA\t* Extra Bedding Available");
+	  getch();
+	 printf("\n\n\t\tSELECT YOUR ROOM TYPE\t");
+	 int room[4]={0,14000,17500,11000},roomt;
+	 scanf("%d",&roomt);
+	 printf("\n\t*\tENTER NO OF ROOMS REQUIRED\t");
+	 scanf("%d",&nrooms);
+	 printf("\n\n\t*\tANY EXTRA BEDDING\t");
+	 scanf("%d",&extrab);
+	  printf("\n\n\t\t->SELECT YOUR MEAL PLAN\t");
+	  int meal[5]={0,0,2000,7000,5500},mealt;
+	  printf("\n\t1-Europeon plan INR 0/-");
+	  printf("\n\n\t2-Continental plan INR 2000/-");
+	  printf("\n\n\t3-American plan INR 7000/-");
+	  printf("\n\n\t4-Modified American plan INR 5500/-\t\t");
+	  scanf("%d",&mealt);
+	  
+	  
+}
+int main()
+{
+	
+	HOMEWOOD_SUITES();
+}
+
